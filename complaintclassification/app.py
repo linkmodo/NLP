057f8a2414512fbd6b69c14cb5f0be4d44818711
Li <175231748+linkmodo@users.noreply.github.com>
2025-03-03 23:53:32 -0500

    Update app.py

--- complaintclassification/app.py
+++ complaintclassification/app.py
@@ -25,7 +25,7 @@ st.markdown(
         background-attachment: fixed;
     }
     [data-testid="stSidebar"] {
-        background-color: rgba(255, 255, 255, 0.2);
+        background-color: rgba(255, 255, 255, 0.1);
         backdrop-filter: blur(10px);
     }
     </style>
