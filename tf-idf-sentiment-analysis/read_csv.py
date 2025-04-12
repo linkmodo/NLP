2bf8bf10c3726e71dcbf032406a042283156f3c4
Li <175231748+linkmodo@users.noreply.github.com>
2025-04-12 10:13:42 -0400

    Delete tf-idf-sentiment-analysis/read_csv.py

--- tf-idf-sentiment-analysis/read_csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Read the first few rows of the CSV file
-df = pd.read_csv("Reddit_Data.csv")
-print("\nColumns in the dataset:", df.columns.tolist())
-print("\nFirst 5 rows of the dataset:")
-print(df.head())
